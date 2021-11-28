86b4d48dba5256533611a07a2a7440b7272ea1c3
Raihan Parama <raihanpl10@gmail.com>
2021-11-28 13:36:08 +0700

    xx

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -77,13 +77,13 @@
         font-weight: bold;
     }
 </style>
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="container">
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark">
+    <div class="container-fluid">
         <a class="navbar-brand"> <img src="{{URL::asset('/image/logo.png')}}" alt="logo" width="300px" height="113px" class="logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse bg-dark" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link {{($title === 'Articles')? 'active' : ''}}" href="/">Home</a>
