7b63b6b7bc71d8428034e87c9c449e5ec9f21701
Raihan Parama <raihanpl10@gmail.com>
2021-12-06 10:37:32 +0700

    update card button on all pages

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -65,10 +65,10 @@
                         <div class="card text-white bg-dark text-center" style="width: 20rem;">
                             <img src="https://source.unsplash.com/1600x800/?{{ $article->category->name }}"
                                 class="card-img-top" alt="...">
-                            <div class="card-body">
+                            <div class="card-body d-flex flex-column">
                                 <h5 class="card-title">{{ $article['title'] }}</h5>
                                 <p class="card-text">{{ $article->excerpt }}</p>
-                                <a href="article/{{ $article->slug }}" class="btn btn-primary"
+                                <a href="article/{{ $article->slug }}" class="btn btn-primary mt-auto"
                                     style="background-color:#00C99C !important; border:none">Read More</a>
                             </div>
                         </div>
