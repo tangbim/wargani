4f3f8d140fc97c7e20f398383ef3c4c9f91e140b
tangbim <santosolintang@gmail.com>
2021-12-08 22:34:27 +0700

    fixing view

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,26 +3,28 @@
         color: white;
         font-family: 'Poppins';
     }
+
 </style>
 @extends('layouts.main')
 
 @section('container')
 
-<body style="background-color: black;">
-    <h1 class="judul mb-5">Post Category: {{ $category }}</h1>
+    <body style="background-color: black;">
+        <h1 class="judul mb-5">Article Category: {{ $category }}</h1>
 
-    <div class=row>
-        @foreach ($articles as $article)
-        <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-items-stretch">
-            <div class="card text-white bg-dark text-center" style="width: 20rem;">
-                <img src="{{ $article->image }}" class="card-img-top" alt="...">
-                <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">{{ $article['title'] }}</h5>
-                    <p class="card-text">{{ $article->excerpt }}</p>
-                    <a href="/article/{{ $article->slug }}" class="btn btn-primary mt-auto" style="background-color:#00C99C !important; border:none">Read More</a>
+        <div class=row>
+            @foreach ($articles as $article)
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-items-stretch">
+                    <div class="card text-white bg-dark text-center" style="width: 20rem;">
+                        <img src="{{ $article->image }}" class="card-img-top" alt="...">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">{{ $article['title'] }}</h5>
+                            <p class="card-text">{{ $article->excerpt }}</p>
+                            <a href="/article/{{ $article->slug }}" class="btn btn-primary mt-auto"
+                                style="background-color:#00C99C !important; border:none">Read More</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
     @endsection
